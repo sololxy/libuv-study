8e472e03871a2710284ef1920750d9884b8382de
sololxy <solo_lxy@126.com>
2017-07-12 16:13:43 +0800

    添加uvcat工程

---
+++ uvcat/uvcat.c
@@ -0,0 +1,79 @@
+﻿/** Copyright luoxiangyong. E-mail: solo_lxy@126.com
+ *
+ * 学习libnv的测试程序，使用nodejs代码库中的libuv源码编译后测试。nodejs代码取之
+ * 这里:https://github.com/nodejs/node ,我使用的是nodejs-8.x。
+ *
+ * 工程管理使用CMake(https://www.cmake.org)。
+ *
+ * libuv的教程参考：http://luohaha.github.io/Chinese-uvbook
+ */
+
+// 包含标准库文件
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+// 包含第三方库文件
+#include <uv.h>
+
+// 包含本项目的文件
+// ..
+
+void on_read(uv_fs_t *req);
+
+uv_fs_t open_req;
+uv_fs_t read_req;
+uv_fs_t write_req;
+
+static char buffer[1024];
+static uv_buf_t iov;
+
+void on_write(uv_fs_t *req) {
+    if (req->result < 0) {
+        fprintf(stderr, "Write error: %s\n", uv_strerror((int)req->result));
+    }
+    else {
+        uv_fs_read(uv_default_loop(), &read_req, open_req.result, &iov, 1, -1, on_read);
+    }
+}
+void on_read(uv_fs_t *req) {
+    if (req->result < 0) {
+        fprintf(stderr, "Read error: %s\n", uv_strerror(req->result));
+    }
+    else if (req->result == 0) {
+        uv_fs_t close_req;
+        // synchronous
+        uv_fs_close(uv_default_loop(), &close_req, open_req.result, NULL);
+    }
+
+    else if (req->result > 0) {
+        iov.len = req->result;
+        uv_fs_write(uv_default_loop(), &write_req, 1, &iov, 1, -1, on_write);
+    }
+}
+
+void on_open(uv_fs_t *req) {
+    // The request passed to the callback is the same as the one the call setup
+    // function was passed.
+    assert(req == &open_req);
+
+    if (req->result >= 0) {
+        iov = uv_buf_init(buffer, sizeof(buffer));
+        uv_fs_read(uv_default_loop(), &read_req, req->result,
+                   &iov, 1, -1, on_read);
+    }
+    else {
+        fprintf(stderr, "error opening file: %s\n", uv_strerror((int)req->result));
+    }
+}
+
+int main(int argc, char **argv) {
+    uv_fs_open(uv_default_loop(), &open_req, argv[1], O_RDONLY, 0, on_open);
+    uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+    uv_fs_req_cleanup(&open_req);
+    uv_fs_req_cleanup(&read_req);
+    uv_fs_req_cleanup(&write_req);
+
+    return 0;
+
+}
